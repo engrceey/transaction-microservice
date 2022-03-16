2c531c224fc4f116b450fce4ff3f784556fe76b5
engrceey <ogbondachristian@gmail.com>
2022-03-17 00:40:38 +0100

    restTemplate config

---
+++ src/main/java/com/reloadly/transactionservice/config/RestTemplateConfiguration.java
@@ -0,0 +1,21 @@
+package com.reloadly.transactionservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+@Configuration
+public class RestTemplateConfiguration {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder
+                .setConnectTimeout(Duration.ofMillis(300000))
+                .setReadTimeout(Duration.ofMillis(300000))
+                .build();
+    }
+}

01877031b12fec516b65a7959ac3300fead43147
engrceey <ogbondachristian@gmail.com>
2022-03-18 20:56:59 +0100

    add load balance

--- src/main/java/com/reloadly/transactionservice/config/RestTemplateConfiguration.java
+++ src/main/java/com/reloadly/transactionservice/config/RestTemplateConfiguration.java
@@ -2,6 +2,7 @@ package com.reloadly.transactionservice.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +13,7 @@ import java.time.Duration;
 public class RestTemplateConfiguration {
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
         return restTemplateBuilder
                 .setConnectTimeout(Duration.ofMillis(300000))
